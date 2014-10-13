1240533fae405378f1dfb56a8979390dce35a33e
Weiyin He <weiyin@users.noreply.github.com>
2014-10-13 12:48:18 -0400

    Update icons and add pngcrush. Fix header image aspect ratio.

--- generate-header-images.sh
+++ generate-header-images.sh
@@ -2,7 +2,12 @@
 
 BASEDIR=$(dirname $0)
 
-sips -z 36 36 -s format png --out $BASEDIR/app/src/main/res/drawable-mdpi/ic_actionbar.png $BASEDIR/HeaderImage 2>&1
-sips -z 54 54 -s format png --out $BASEDIR/app/src/main/res/drawable-hdpi/ic_actionbar.png $BASEDIR/HeaderImage 2>&1
-sips -z 72 72 -s format png --out $BASEDIR/app/src/main/res/drawable-xhdpi/ic_actionbar.png $BASEDIR/HeaderImage 2>&1
-sips -z 108 108 -s format png --out $BASEDIR/app/src/main/res/drawable-xxhdpi/ic_actionbar.png $BASEDIR/HeaderImage 2>&1
+sips --resampleHeight 36 -s format png --out $BASEDIR/app/src/main/res/drawable-mdpi/ic_actionbar.png $BASEDIR/HeaderImage 2>&1
+sips --resampleHeight 54 -s format png --out $BASEDIR/app/src/main/res/drawable-hdpi/ic_actionbar.png $BASEDIR/HeaderImage 2>&1
+sips --resampleHeight 72 -s format png --out $BASEDIR/app/src/main/res/drawable-xhdpi/ic_actionbar.png $BASEDIR/HeaderImage 2>&1
+sips --resampleHeight 108 -s format png --out $BASEDIR/app/src/main/res/drawable-xxhdpi/ic_actionbar.png $BASEDIR/HeaderImage 2>&1
+
+pngcrush -q -rem -allb -brute -reduce -ow $BASEDIR/app/src/main/res/drawable-mdpi/ic_actionbar.png 2>&1
+pngcrush -q -rem -allb -brute -reduce -ow $BASEDIR/app/src/main/res/drawable-hdpi/ic_actionbar.png 2>&1
+pngcrush -q -rem -allb -brute -reduce -ow $BASEDIR/app/src/main/res/drawable-xhdpi/ic_actionbar.png 2>&1
+pngcrush -q -rem -allb -brute -reduce -ow $BASEDIR/app/src/main/res/drawable-xxhdpi/ic_actionbar.png 2>&1
