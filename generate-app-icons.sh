14bd9a46bf4d2c6971abdba04325e0c2d43f8b86
Weiyin He <weiyin@users.noreply.github.com>
2014-11-30 17:33:18 -0500

    fix generate-appi-cons.sh

--- generate-app-icons.sh
+++ generate-app-icons.sh
@@ -3,11 +3,11 @@
 BASEDIR=$(dirname $0)
 sips -z 1024 1024 -s format png --out $BASEDIR/AppIconTemp.png $BASEDIR/AppIcon 2>&1
 
-convert -size 1024x1024 xc:none -draw "roundrectangle 0,0,1024,1024,80,80" mask.png
-convert AppIconTemp.png -matte mask.png -compose DstIn -composite AppIcon
+convert -size 1024x1024 xc:none -draw "roundrectangle 0,0,1024,1024,80,80" $BASEDIR/mask.png
+convert $BASEDIR/AppIconTemp.png -matte $BASEDIR/mask.png -compose DstIn -composite $BASEDIR/AppIcon
 
-rm AppIconTemp.png
-rm mask.png
+rm $BASEDIR/AppIconTemp.png
+rm $BASEDIR/mask.png
 
 sips -z 48 48 -s format png --out $BASEDIR/app/src/main/res/drawable-mdpi/ic_launcher.png $BASEDIR/AppIcon 2>&1
 sips -z 72 72 -s format png --out $BASEDIR/app/src/main/res/drawable-hdpi/ic_launcher.png $BASEDIR/AppIcon 2>&1
